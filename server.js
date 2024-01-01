eb5ed78f4ec99cb2f69869ad1e1a093adfbe65c5
Kdddru <125441996+Kdddru@users.noreply.github.com>
2024-01-01 23:50:52 +0900

    0101 put, delete

--- server.js
+++ server.js
@@ -32,16 +32,25 @@ const users= [
 ]
 
 
-
+//값들고오기
 app.get('/api/user',(req,res)=>{
   res.json(users);
-})
+});
 
+//새로운 값 생성하기
+app.post('/api/user',(req, res)=>{
+  res.json(users);
+  console.log(req.body);
+});
+
+
+//param값들고오기
 app.get('/api/user/:id',(req,res)=>{
   const {id} = req.params
   const user = users.find((u)=>(
     u.id === id
-  ))
+  ));
+
   console.log(user);
   if(user){
     res.json(user);
@@ -52,6 +61,40 @@ app.get('/api/user/:id',(req,res)=>{
 });
 
 
-app.listen(port, ()=>{
-  console.log('서버 실행')
+//업데이트
+app.put('/api/user/:id',(req, res)=>{
+  const {id} = req.params;
+  let foundIndex= users.findIndex((u)=>u.id === id);
+  console.log(foundIndex)
+  if(foundIndex === -1){
+    res.status(404).json({error : 'undefined !'});
+  }
+  else{
+    users[foundIndex] = {...users[foundIndex], ...req.body};
+    res.json(users[foundIndex]);
+  }
 })
+
+//삭제
+app.delete('/api/user/:id', (req,res)=>{
+  const {id} =req.params;
+  let foundIndex= users.findIndex((u)=>u.id === id);
+  if(foundIndex === -1){
+    res.status(404).json({error : 'undefined !'});
+  }
+  else{
+    let foundUser = users.splice(foundIndex,1);
+    res.json(foundUser[0]);
+  }
+
+})
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+  console.log('서버 실행');
+});
